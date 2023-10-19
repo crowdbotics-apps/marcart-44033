a06a1fb79e28a8d35b6f33518cf3888a14bfbd50
Crowdbotics <team@crowdbotics.com>
2023-10-19 08:38:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "marcart-44033",
+  url: "https://marcart-44033.botics.co",
+  api: "https://marcart-44033.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
